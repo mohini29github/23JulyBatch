b7ee0707fa685c2626d24a2c545f3e1e7df2baf3
unknown <zolmohini297@gmail.com>
2022-09-20 12:33:27 +0530

    adiing JavaArray

---
+++ JavaArray/src/com/test/array/Demo1.java
@@ -0,0 +1,22 @@
+package com.test.array;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		String[] str= {"v","e","l","o","c","i","t","y"};
+		
+		//for loop
+		for(int i=0;i<str.length;i++) {
+			System.out.print(str[i]);
+	}	
+		//for-each loop
+			for(String j:str) {
+				System.out.print(j);
+			
+		}
+		
+		
+	}
+
+}
